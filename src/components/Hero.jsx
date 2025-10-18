9e74f0a47d79f044b4e334eb4d9a7e9c0656afde
hatifshaikh88 <hatifshaikh88732@gmail.com>
2025-10-18 22:28:31 +0530

    Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from '../pages/home'
+import Home from '../pages/Home'
 import { motion } from 'framer-motion'
 import ProfileImage from '../assets/ProfileImage.jpeg'
 const Hero = () => {
